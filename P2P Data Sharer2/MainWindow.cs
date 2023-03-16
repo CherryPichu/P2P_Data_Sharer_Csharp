bbece0614b258e32c8adeab8757604c7a91ef9d8
CherryPichu <uskawjdu@gmail.com>
2023-03-17 00:25:36 +0900

    초기 커밋.

---
+++ P2P Data Sharer2/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Gtk;
+namespace P2P_Data_Sharer2
+{
+    public class MainWindow : Window
+    {
+        private HeaderBar _headerBar;
+
+        public MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+        {
+            _headerBar = builder.GetObject("HeaderBar") as HeaderBar;
+            _headerBar.ShowCloseButton = true;
+
+            DeleteEvent += (sender, args) => Application.Quit();
+        }
+
+        public MainWindow() : this(new Builder("MainWindow.glade"))
+        {
+        }
+    }
+   } 
---
+++ P2P Data Sharer2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.GTK;
+using Gtk;
+using P2P_Data_Sharer2;
+
+namespace GameOfLife.GTK
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+
+            // Load the Glade file
+            //var builder = new Builder("MainWindow.glade");
+            //builder.AddFromFile("./MainWindow.glade");
+
+            // Get the objects from the Glade file
+            //var window = (Window)builder.GetObject("main_window");
+            //var window = new MainWindow(builder);
+            // Connect the button click event
+            IntPtr appAddress = GetApplicationAddressFromAnotherProcess();
+            var app = new Application(appAddress);
+
+            var win = new MainWindow();
+            app.AddWindow(win);
+
+            win.Show();
+            Application.Run();
+
+        }
+        static IntPtr GetApplicationAddressFromAnotherProcess()
+        {
+            // TODO: Implement method to get the address of the existing GTK# Application object
+            return IntPtr.Zero;
+        }
+    }
+}
